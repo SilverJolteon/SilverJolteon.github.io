a45504015e5116c9ba2e270bf80c6707dbc893a2
SilverJolteon <silverchrysaetos@live.com>
2020-10-07 21:28:18 -0600

    Added menu + Skin Colors (Beta)

--- ACNH/editor/scripts/color.js
+++ ACNH/editor/scripts/color.js
@@ -28,6 +28,10 @@ function color_32_to_8(red_32, green_32, blue_32){
 	if(swap32(green_32) > 0x3F000000) green_32 = 0x0000003F;
 	if(swap32(blue_32) > 0x3F000000) blue_32 = 0x0000003F;
 	
+	if(swap32(red_32) < 0x3C000000) red_32 = 0x000000CF;
+	if(swap32(green_32) < 0x3C000000) green_32 = 0x000000CF;
+	if(swap32(blue_32) < 0x3C000000) blue_32 = 0x000000CF;
+	
 	var red_8 = swap32(red_32) * 5.0663948059082E-6 - 5100;
 	var green_8 = swap32(green_32) * 5.0663948059082E-6 - 5100;
 	var blue_8 = swap32(blue_32) * 5.0663948059082E-6 - 5100;
@@ -37,6 +41,7 @@ function color_32_to_8(red_32, green_32, blue_32){
 
 function getColor(color_id_old, type){
 	var color_id = color_id_old;
+	var skin_color = {};
 	var hair_color = {};
 	var shine_color = {};
 	var eye_color = {};
@@ -45,6 +50,14 @@ function getColor(color_id_old, type){
 	for(i = 0; i < 4; i++){
 		var temp;
 		if(type == 0){
+			temp = CharaMakeSkinColorParam[0xB0 + (color_id * 0x78) + 3 - i]&0xFF>>>0;
+			skin_color.blue_32 |= temp << i * 8;
+			temp = CharaMakeSkinColorParam[0xB0 + (color_id * 0x78) + 4 + 3 - i]&0xFF>>>0;
+			skin_color.green_32 |= temp << i * 8;
+			temp = CharaMakeSkinColorParam[0xB0 + (color_id * 0x78) + 8 + 3 - i]&0xFF>>>0;
+			skin_color.red_32 |= temp << i * 8;
+		}
+		if(type == 1){
 			color_id = color_ids[color_id_old];
 			temp = CharaMakeHairColorParam[0x74 + (color_id * 0x60) + 3 - i]&0xFF>>>0;
 			hair_color.red_32 |= temp << i * 8;
@@ -60,7 +73,7 @@ function getColor(color_id_old, type){
 			temp = CharaMakeHairColorParam[0x68 + (color_id * 0x60) + 8 + 3 - i]&0xFF>>>0;
 			shine_color.red_32 |= temp << i * 8;
 		}
-		if(type == 1){
+		if(type == 2){
 			temp = CharaMakeEyeColorParam[0x50 + (color_id * 0x54) + 3 - i]&0xFF>>>0;
 			eye_color.blue_32 |= temp << i * 8;
 			temp = CharaMakeEyeColorParam[0x50 + (color_id * 0x54) + 4 + 3 - i]&0xFF>>>0;
@@ -69,13 +82,26 @@ function getColor(color_id_old, type){
 			eye_color.red_32 |= temp << i * 8;
 		}
 	}
-	if(type == 0){
+	if(type == 0) color.skin = color_32_to_8(skin_color.red_32, skin_color.green_32, skin_color.blue_32);
+	if(type == 1){
 		color.hair = color_32_to_8(hair_color.red_32, hair_color.green_32, hair_color.blue_32);
 		color.shine = color_32_to_8(shine_color.red_32, shine_color.green_32, shine_color.blue_32);
 	}
-	if(type == 1) color.eye = color_32_to_8(eye_color.red_32, eye_color.green_32, eye_color.blue_32);
+	if(type == 2) color.eye = color_32_to_8(eye_color.red_32, eye_color.green_32, eye_color.blue_32);
 	
 	return color;
+}
+
+function edit_skin(color_id, skin){
+	var skin_color = color_8_to_32(skin);
+	
+	// Replace bytes
+	
+	for(i = 0; i < 4; i++){
+		CharaMakeSkinColorParam[0xB0 + (color_id * 0x78) + 3 - i] = (skin_color.blue_32 >> (i * 8) & 0xFF);
+		CharaMakeSkinColorParam[0xB0 + (color_id * 0x78) + 4 + 3 - i] = (skin_color.green_32 >> (i * 8) & 0xFF);
+		CharaMakeSkinColorParam[0xB0 + (color_id * 0x78) + 8 + 3 - i] = (skin_color.red_32 >> (i * 8) & 0xFF);
+	}
 }
 
 function edit_hair(color_id, hair, shine){
@@ -114,6 +140,30 @@ function pad(str, num, c){
 
 jscolor.trigger('input');
 
+function gen_skin(color_id) {
+		var isChrome = navigator.userAgent.indexOf("Chrome") != -1;
+		var size = 6;
+		if(isChrome) size = 9;
+		var p0 = document.createElement('center');
+		var p1 = document.createElement('tr');
+		p0.appendChild(p1);
+		var skin = document.createElement('INPUT');
+		skin.size = size;
+		skin.id = "skin-color-" + color_id;
+		p1.appendChild(skin);
+		new JSColor(skin, {hash:true});
+		var color = getColor(color_id, 0);
+		skin.jscolor.fromString(pad(color.skin.toString(16), 6, '0'));	
+		var rst = document.createElement('button');
+		rst.innerHTML = '&#x21bb;';
+		rst.className = "reset";
+		rst.onclick = function(){skin.jscolor.fromString(pad(color.skin.toString(16), 6, '0'));}
+		p1.appendChild(rst);		
+		if(color_id < 4) document.getElementById('skin_colors0').appendChild(p0);
+		else if(color_id < 8) document.getElementById('skin_colors1').appendChild(p0);
+		else document.getElementById('skin_colors2').appendChild(p0);
+}
+
 function gen_hair(color_id) {
 		var isChrome = navigator.userAgent.indexOf("Chrome") != -1;
 		var size = 6;
@@ -131,7 +181,7 @@ function gen_hair(color_id) {
 		p1.appendChild(shine);
 		new JSColor(hair, {hash:true});
 		new JSColor(shine, {hash:true});
-		var color = getColor(color_id, 0);
+		var color = getColor(color_id, 1);
 		hair.jscolor.fromString(pad(color.hair.toString(16), 6, '0'));	
 		shine.jscolor.fromString(pad(color.shine.toString(16), 6, '0'));
 		var rst = document.createElement('button');
@@ -158,7 +208,7 @@ function gen_eye(color_id) {
 		eye.id = "eye-color-" + color_id;
 		p1.appendChild(eye);
 		new JSColor(eye, {hash:true});
-		var color = getColor(color_id, 1);
+		var color = getColor(color_id, 2);
 		eye.jscolor.fromString(pad(color.eye.toString(16), 6, '0'));	
 		var rst = document.createElement('button');
 		rst.innerHTML = '&#x21bb;';
--- ACNH/editor/scripts/menu.js
+++ ACNH/editor/scripts/menu.js
@@ -4,9 +4,10 @@ window.onload = function(){
 		var settings = document.getElementById("settings");
 		settings.style["right"] = "10px";
 	}
-	
+	for(var id = 0; id < 12; id++) gen_skin(id);
 	for(var id = 0; id < 16; id++) gen_hair(id);
 	for(var id = 0; id < 12; id++) gen_eye(id);
+	select_menu(1);
 }
 
 var saveByteArray = (function () {
@@ -22,6 +23,18 @@ var saveByteArray = (function () {
         window.URL.revokeObjectURL(url);
     };
 }());
+
+function save_skin(){
+	for(var id = 0; id < 12; id++){
+		var skin = document.getElementById("skin-color-" + id);
+		skin = parseInt(skin.value.substring(1, 7), 16);
+	
+		edit_skin(id, skin);
+	}
+	saveByteArray([CharaMakeSkinColorParam], 'CharaMakeSkinColorParam.bcsv');
+	var location = document.getElementById("skin_location");
+	location.innerHTML = 'Save to <font color="#34ADCA">/atmosphere/contents/01006F8002326000/romfs/Bcsv/</font><font color="#017A97">CharaMakeSkinColorParam.bcsv</font>';
+}
 
 function save_hair(){
 	for(var id = 0; id < 16; id++){
@@ -51,18 +64,24 @@ function save_eye(){
 
 function save_settings(){
 	var data = [];
-	data.push("Presets");
+	data.push("Skin Colors");
+	for(var i = 0; i < 12; i++){
+		var skin = document.getElementById("skin-color-" + i);
+		data.push("\n"+skin.value);
+	}
+	data.push("\n,\nHair Colors");
 	for(var i = 0; i < 16; i++){
 		var hair = document.getElementById("hair-color-" + color_ids[i]);
 		var shine = document.getElementById("shine-color-" + color_ids[i]);
 		data.push("\n"+hair.value);
 		data.push(shine.value);
 	}
+	data.push("\n,\nEye Colors");
 	for(var i = 0; i < 12; i++){
 		var eye = document.getElementById("eye-color-" + i);
-		if(i == 0) data.push("\n\n"+eye.value);
-		else data.push("\n"+eye.value);
+		data.push("\n"+eye.value);
 	}
+	data.push("\n,");
 	saveByteArray([data], 'presets.csv');
 }
 
@@ -72,18 +91,56 @@ var loadSettings = function(event){
 	reader.readAsText(file);
 	reader.onload = function(){
 		var buffer = reader.result, data = buffer.split(",");
-		if(data[0].localeCompare("Presets") == 0){
-			for(var i = 0; i < 16; i++){
-			var hair = document.getElementById("hair-color-" + color_ids[i]);
-			var shine = document.getElementById("shine-color-" + color_ids[i]);
-			hair.jscolor.fromString(data[i*2 + 1]);	
-			shine.jscolor.fromString(data[i*2 + 2]);
+		if(data[0].localeCompare("Skin Colors") == 0){
+			var line = 1;
+			var i = 0;
+			while(1){
+				if(data[line].localeCompare("\n") == 0) break;
+				var skin = document.getElementById("skin-color-" + i);
+				skin.jscolor.fromString(data[line]);
+				line++;
+				i++;
 			}
-			for(var i = 0; i < 12; i++){
+			
+			line += 2;
+			i = 0;
+			while(1){
+				if(data[line].localeCompare("\n") == 0) break;
+				var hair = document.getElementById("hair-color-" + color_ids[i]);
+				var shine = document.getElementById("shine-color-" + color_ids[i]);
+				hair.jscolor.fromString(data[line]);	
+				shine.jscolor.fromString(data[line + 1]);
+				line+=2;
+				i++;
+			}
+			line += 2;
+			i = 0;
+			while(1){
+				if(data[line].localeCompare("\n") == 0) break;
 				var eye = document.getElementById("eye-color-" + i);
-				eye.jscolor.fromString(data[33 + i]);
+				eye.jscolor.fromString(data[line]);
+				line++;
+				i++;
 			}
 		}
 		
 	};
 };
+
+function select_menu(n){
+	for(var i = 0 ; i < 3; i++){
+		document.getElementById("map").src="assets/menu-bg-" + n + ".png";
+		document.getElementById("skin_location").innerHTML = "";
+		document.getElementById("hair_location").innerHTML = "";
+		document.getElementById("eye_location").innerHTML = "";
+
+		if(n == i){
+			document.getElementById("menu" + i).style.display = "block";
+			document.getElementById("body" + i).style.display = "block";
+		}
+		else{
+			document.getElementById("menu" + i).style.display = "none";
+			document.getElementById("body" + i).style.display = "none";
+		}
+	}
+}
