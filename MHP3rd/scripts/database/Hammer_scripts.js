9109852a09e7eaf1ab846f38f0536b40b1f5b908
Silver <silverchrysaetos@live.com>
2024-04-14 18:11:10 -0600

    Moved to mhp3db.github.io

--- MHP3rd/scripts/database/Hammer_scripts.js
+++
@@ -1,6 +0,0 @@
-function Hammer_init(){
-	data = Hammer_data;
-	var header = document.getElementById("unique-header");
-	header.innerHTML = "";
-	header.style.display = "none";
-}
--- MHP3rd/scripts/database/LS_scripts.js
+++
@@ -1,6 +0,0 @@
-function LS_init(){
-	data = LS_data;
-	var header = document.getElementById("unique-header");
-	header.innerHTML = "";
-	header.style.display = "none";
-}
