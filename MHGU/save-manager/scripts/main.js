c0c08ed3aa04e2bf4194704f44d1f45a9aaedf0f
Silver <silverchrysaetos@live.com>
2024-10-07 20:06:41 -0600

    Update main.js

--- MHGU/save-manager/scripts/main.js
+++ MHGU/save-manager/scripts/main.js
@@ -205,7 +205,7 @@ function displayInfo(save) {
     var text = `<select class="dropdown" id="dropdown">`;
 	text += `<option value=0>MHXX 3DS</option>`;
 	text += `<option value=1>MHGU Switch</option>`;
-	text += `</select><span class="menu"><button onclick="downloadSave()">Download (With DLC)</button></span>`;
+	text += `</select><span class="menu"><button onclick="downloadSave()">Export save (With DLC)</button></span>`;
 	DL.innerHTML = text;
 	
     var table = document.getElementById("saveTable");
